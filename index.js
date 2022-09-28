627da1131bf85e49ed1626fa4fb0a0232bcb54e2
Daniel Paes <daniel@gscrd.com.br>
2022-09-28 13:51:40 -0300

    finançasFinalizado

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let Quantity_initial_money = prompt("Digite quantidade inicial de saldo: ")
+Quantity_initial_money = parseFloat(Quantity_initial_money)
 let option = ""
 do {
     option = prompt("Escolha uma das opções: " +
@@ -8,4 +9,19 @@ do {
     "\n\nSaldo Disponível: " + Quantity_initial_money
     )
     
+    switch (option){
+        case "1":
+            Quantity_initial_money += parseFloat( prompt("Digite valor a ser adicionado: "))
+            break
+        case "2":
+            Quantity_initial_money -= parseFloat( prompt("Digite valor a ser removido: "))
+            break
+        case "3":
+            alert("Saindo...")
+            break
+
+        default:
+            alert("Opção Inválida.")
+    }
+
 } while (option !== "3")
