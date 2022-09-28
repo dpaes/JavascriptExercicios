43442eae8dd140ed46b43e9aefdf2fd3118dc041
Daniel Paes <daniel@gscrd.com.br>
2022-09-28 13:12:34 -0300

    MENU FINALIZADO

--- index.js
+++ index.js
@@ -10,4 +10,31 @@ do {
         "\n4- Opção 4" +
         "\n5- Sair"
     )
+
+    switch (option) {
+        case "1":
+            alert("Você escolheu a opção 1")
+            break
+
+        case "2":
+            alert("Você escolheu a opção 2")
+            break
+        
+        case "3":
+            alert("Você escolheu a opção 3")
+            break
+        
+        case "4":
+            alert("Você escolheu a opção 4")
+            break
+        
+        case "5":
+            alert("Você escolheu encerrar.")
+            alert("Encerrando...")
+            break
+
+        default:
+            alert("Opção Inválida.")
+    }
+
 } while (option !== "5")
