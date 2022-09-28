40df130a609d80f45a17029caec8be7e6544458f
Daniel Paes <daniel@gscrd.com.br>
2022-09-28 16:56:28 -0300

    palin Final att

--- Palíndromo/palin.js
+++ Palíndromo/palin.js
@@ -2,4 +2,12 @@ const word = prompt("Digite uma palavra: ")
 
 let invertWord = ""
 
-for (let i = palavra.length - 1)
+for (let i = word.length - 1;i >= 0; i--){
+    invertWord += word[i]
+}
+
+if(word == invertWord) {
+    alert(word + " é um Palíndromo!")
+} else {
+    alert(word + " Não é um Palíndromo!")
+}
