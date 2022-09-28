248cd6b73fa3fd5f76fb067159e58a57415ff8f1
Daniel Paes <daniel@gscrd.com.br>
2022-09-28 13:29:08 -0300

    controleFinanceiro2

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
-let option = "5"
-
+let Quantity_initial_money = prompt("Digite quantidade inicial de saldo: ")
+let option = ""
 do {
-    option = prompt(
-        "Seja bem vindo(a)\n" +
-        "\nEscolha uma das opções abaixo: " +
-        "\n1- Opção 1" +
-        "\n2- Opção 2" +
-        "\n3- Opção 3" +
-        "\n4- Opção 4" +
-        "\n5- Sair"
+    option = prompt("Escolha uma das opções: " +
+    "\n1-Adicionar dinheiro" + 
+    "\n2-Remover dinheiro" +
+    "\n3-SAIR" +
+    "\n\nSaldo Disponível: " + Quantity_initial_money
     )
-
-    switch (option) {
-        case "1":
-            alert("Você escolheu a opção 1")
-            break
-
-        case "2":
-            alert("Você escolheu a opção 2")
-            break
-        
-        case "3":
-            alert("Você escolheu a opção 3")
-            break
-        
-        case "4":
-            alert("Você escolheu a opção 4")
-            break
-        
-        case "5":
-            alert("Você escolheu encerrar.")
-            alert("Encerrando...")
-            break
-
-        default:
-            alert("Opção Inválida.")
-    }
-
-} while (option !== "5")
+    
+} while (option !== "3")
