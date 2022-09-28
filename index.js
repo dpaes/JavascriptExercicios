67261c7d98c24049f28f621a3cea55f7ef452fc5
Daniel Paes <daniel@gscrd.com.br>
2022-09-28 13:05:09 -0300

    mud

---
+++ index.js
@@ -0,0 +1,13 @@
+let option = "5"
+
+do {
+    option = prompt(
+        "Seja bem vindo(a)\n" +
+        "\nEscolha uma das opções abaixo: " +
+        "\n1- Opção 1" +
+        "\n2- Opção 2" +
+        "\n3- Opção 3" +
+        "\n4- Opção 4" +
+        "\n5- Sair"
+    )
+} while (option !== "5")
